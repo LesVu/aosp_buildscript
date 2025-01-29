ea8d13d51b3a9b800d07520b6a92092afc33e049
LesVu <66196443+LesVu@users.noreply.github.com>
2025-01-29 15:36:06 +0800

    Update build_rosemary_eng.sh

--- lineageos/build_rosemary_eng.sh
+++ lineageos/build_rosemary_eng.sh
@@ -7,16 +7,10 @@ wget "https://raw.githubusercontent.com/LesVu/aosp_buildscript/refs/heads/main/l
 source build/envsetup.sh
 breakfast lineage_rosemary-ap4a-eng
 
-rm kernel/xiaomi/mt6785/arch/arm64/configs/rosemary_defconfig
-wget https://raw.githubusercontent.com/LesVu/android_kernel_xiaomi_mt6785/refs/heads/lineage-$LINEAGE_VERSION/arch/arm64/configs/rosemary-mod_defconfig -O kernel/xiaomi/mt6785/arch/arm64/configs/rosemary_defconfig
-
 echo "persist.sys.usb.config=adb
 sys.usb.config=adb
 sys.usb.state=adb
 vendor.usb.config=adb" >>device/xiaomi/rosemary/system.prop
 
-sed -i '/# CONFIG_SYSVIPC is not set/d' kernel/configs/r/android-4.14/android-base.config
-sed -i '/# CONFIG_FHANDLE is not set/d' kernel/configs/r/android-4.14/android-base.config
-sed -i '/# CONFIG_IP6_NF_NAT is not set/d' kernel/configs/r/android-4.14/android-base.config
 croot
 brunch lineage_rosemary-ap4a-eng
