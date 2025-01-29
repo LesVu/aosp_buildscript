3e42348cbb6e07513c36f8e90d624c8dcb6a90f7
LesVu <66196443+LesVu@users.noreply.github.com>
2025-01-29 15:36:54 +0800

    Update build_rosemary.sh

--- lineageos/build_rosemary.sh
+++ lineageos/build_rosemary.sh
@@ -6,12 +6,5 @@ wget "https://raw.githubusercontent.com/LesVu/aosp_buildscript/refs/heads/main/l
 /opt/crave/resync.sh
 source build/envsetup.sh
 breakfast rosemary
-
-rm kernel/xiaomi/mt6785/arch/arm64/configs/rosemary_defconfig
-wget https://raw.githubusercontent.com/LesVu/android_kernel_xiaomi_mt6785/refs/heads/lineage-$LINEAGE_VERSION/arch/arm64/configs/rosemary-mod_defconfig -O kernel/xiaomi/mt6785/arch/arm64/configs/rosemary_defconfig
-
-sed -i '/# CONFIG_SYSVIPC is not set/d' kernel/configs/r/android-4.14/android-base.config
-sed -i '/# CONFIG_FHANDLE is not set/d' kernel/configs/r/android-4.14/android-base.config
-sed -i '/# CONFIG_IP6_NF_NAT is not set/d' kernel/configs/r/android-4.14/android-base.config
 croot
 brunch rosemary
