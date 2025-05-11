a707eca0b76480c3566b3fcbde9aa5e2a35f51a8
LesVu <johndie005@gmail.com>
2025-05-12 00:27:27 +0800

    Updated

--- lineageos/build_rosemary_eng.sh
+++ lineageos/build_rosemary_eng.sh
@@ -3,7 +3,7 @@ LINEAGE_VERSION=22.1
 rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 wget "https://raw.githubusercontent.com/LesVu/aosp_buildscript/refs/heads/main/lineageos/rosemary-${LINEAGE_VERSION}.xml" -O .repo/local_manifests/rosemary.xml
-/opt/crave/resync.sh || repo sync
+/opt/crave/resync.sh || repo sync -c
 # shellcheck source=/dev/null
 source build/envsetup.sh
 
